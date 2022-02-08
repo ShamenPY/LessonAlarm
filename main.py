e7d51a138c2f7d408950550c4c02736f6ae480d2
ShamenPY <karolswwacha17@gmail.com>
2022-02-08 19:17:56 +0100

    .3

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-file = open(r"C:\Users\a\Desktop\Lesson-Alarm\Lesson plan.txt")
+file = open(r"Lesson plan.txt")
 import datetime
-import pyttsx3
-import time
 from time import sleep
-
 for i in file:
 
-    list = i.split()
+    line_list = i.split()
 
+    if line_list[0] == "Monday":
+        list_of_monday = line_list[1:]
+    elif line_list[0] == "Tuesday":
+        list_of_tuesday = line_list[1:]
+    elif line_list[0] == "Wednesday":
+        list_of_wednesday = line_list[1:]
+    elif line_list[0] == "Thursday":
+        list_of_thursday = line_list[1:]
+    elif line_list[0] == "Friday":
+        list_of_friday = line_list[1:]
+    elif line_list[0] == "lesson_alarm":
+        lesson_alarm = line_list[1:]
 
-    if list[0] == "Monday":
-        list_of_monday = list[1:]
-    elif list[0] =="Tuesday":
-        list_of_tuesday = list[1:]
-    elif list[0] == "Wednesday":
-        list_of_wednesday = list[1:]
-    elif list[0] == "Thursday":
-        list_of_thursday = list[1:]
-    elif list[0] == "Friday":
-        list_of_friday = list[1:]
-    elif list[0] == "lesson_alarm":
-        lesson_alarm = list[1:]
 while True:
     sleep(1)
     now = datetime.datetime.now()
@@ -29,10 +26,6 @@ while True:
     print(now, day)
     time = now.strftime("%H.%M")
     print(time)
-    engine = pyttsx3.init()
-    engine.setProperty("voice", "com.apple.speech.synthesis.voice.zosia.premium")
-    rate = engine.getProperty("rate")
-    engine.runAndWait()
 
 
     if time in lesson_alarm:
@@ -42,8 +35,6 @@ while True:
             case "MONDAY":
                 index_of_alarm = lesson_alarm.index(time)
                 print(list_of_monday[index_of_alarm])
-                tekst = "Rozpoczeła sie lekcja " + list_of_monday[index_of_alarm]
-                engine.say(tekst)
             case "TUESDAY":
                 index_of_alarm = lesson_alarm.index(time)
                 print(list_of_tuesday[index_of_alarm])
@@ -58,12 +49,10 @@ while True:
                 print(list_of_friday[index_of_alarm])
         sleep(60)
 
-
-
-
-
-
 print(list)
 file.close()
 
 
+
+
+ 
