a607cc1bb02bd7b2d758a0ec25974f3a9b7b3f46
ShamenPY <karolswwacha17@gmail.com>
2022-02-11 12:59:46 +0100

    .3

--- main.py
+++ main.py
@@ -2,12 +2,51 @@ file = open(r"Lesson plan.txt")
 import datetime
 from time import sleep
 from plyer import notification
-
+from tkinter import *
+import tkinter as tk
+#import tkinter as tk
+from tkinter import messagebox
 #Tkinker
 
 
+root = tk.Tk()
+root.title("CONFIG - Lesson Alarm")
+root.geometry("500x300")
 
+root.configure(background='#d4d4d4')
+root.geometry("400x300")
+l3=tk.Label(root,text="dzwonki", bg="#d4d4d4")
+l3.grid(row=1,column=1)
+l1=tk.Label(root,text="Monday",bg="#d4d4d4")
+l1.grid(row=2,column=1)
+l2=tk.Label(root,text="Tuesday",bg="#d4d4d4")
+l2.grid(row=3,column=1)
+l3=tk.Label(root,text="Wednesday", bg="#d4d4d4")
+l3.grid(row=4,column=1)
+l3=tk.Label(root,text="Thursday", bg="#d4d4d4")
+l3.grid(row=5,column=1)
+l3=tk.Label(root,text="Friday", bg="#d4d4d4")
+l3.grid(row=6,column=1)
+l3=tk.Label(root,text="Wyprzedzenie: ", bg="#d4d4d4")
+l3.grid(row=7,column=1)
 
+alarm1,alarm2,alarm3,alarm4,alarm5,alarm6,alarm7,alarm8 = Entry(root, width=5,).grid(row=1,column=2), Entry(root, width=5).grid(row=1,column=3), Entry(root, width=5).grid(row=1,column=4), Entry(root, width=5).grid(row=1,column=5), Entry(root, width=5).grid(row=1,column=6), Entry(root, width=5).grid(row=1,column=7), Entry(root, width=5).grid(row=1,column=8), Entry(root, width=5).grid(row=1,column=9),
+monday1,monday2,monday3,monday4,monday5,monday6,monday7,monday8 = Entry(root, width=5).grid(row=2,column=2), Entry(root, width=5).grid(row=2,column=3), Entry(root, width=5).grid(row=2,column=4), Entry(root, width=5).grid(row=2,column=5), Entry(root, width=5).grid(row=2,column=6), Entry(root, width=5).grid(row=2,column=7), Entry(root, width=5).grid(row=2,column=8), Entry(root, width=5).grid(row=2,column=9),
+tuesday1,tuesday2,tuesday3,tuesday4,tuesday5,tuesday6,tuesday7,tuesday8 = Entry(root, width=5).grid(row=3,column=2), Entry(root, width=5).grid(row=3,column=3), Entry(root, width=5).grid(row=3,column=4), Entry(root, width=5).grid(row=3,column=5), Entry(root, width=5).grid(row=3,column=6), Entry(root, width=5).grid(row=3,column=7), Entry(root, width=5).grid(row=3,column=8), Entry(root, width=5).grid(row=3,column=9),
+wednesday1,wednesday2,wednesday3,wednesday4,wednesday5,wednesday6,wednesday7,wednesday8 = Entry(root, width=5).grid(row=4,column=2), Entry(root, width=5).grid(row=4,column=3), Entry(root, width=5).grid(row=4,column=4), Entry(root, width=5).grid(row=4,column=5), Entry(root, width=5).grid(row=4,column=6), Entry(root, width=5).grid(row=4,column=7), Entry(root, width=5).grid(row=4,column=8), Entry(root, width=5).grid(row=4,column=9),
+thursday1,thursday2,thursday3,thursday4,thursday5,thursday6,thursday7,thursday8 = Entry(root, width=5).grid(row=5,column=2), Entry(root, width=5).grid(row=5,column=3), Entry(root, width=5).grid(row=5,column=4), Entry(root, width=5).grid(row=5,column=5), Entry(root, width=5).grid(row=5,column=6), Entry(root, width=5).grid(row=5,column=7), Entry(root, width=5).grid(row=5,column=8), Entry(root, width=5).grid(row=5,column=9),
+friday1,friday2,friday3,friday4,friday5,friday6,friday7,friday8 = Entry(root, width=5).grid(row=6,column=2), Entry(root, width=5).grid(row=6,column=3), Entry(root, width=5).grid(row=6,column=4), Entry(root, width=5).grid(row=6,column=5), Entry(root, width=5).grid(row=6,column=6), Entry(root, width=5).grid(row=6,column=7), Entry(root, width=5).grid(row=6,column=8), Entry(root, width=5).grid(row=6,column=9),
+wyprzedzenie = Entry(root, width=5).grid(row=7,column=2)
+
+def submit():
+    global alarm1,alarm2,alarm3
+    alarm1 = alarm1.get()
+    print(alarm1)
+
+submitButton = Button(root, text="Zapisz", command=submit).grid(row=8,column=1)
+
+root.mainloop()
+print(alarm1)
 
 def notifyMe(title, message):
     notification.notify(
@@ -65,25 +104,21 @@ while True:
             case "WEDNESDAY":
                 index_of_alarm = lesson_alarm.index(time)
                 print(list_of_wednesday[index_of_alarm])
-                print(list_of_tuesday[index_of_alarm])
                 notifyMe(f"ROZPOCZĘŁA SIĘ {(list_of_wednesday[index_of_alarm])} ", "Dołącz na lekcję jak najszybciej!")
             case "THURSDAY":
                 index_of_alarm = lesson_alarm.index(time)
                 print(list_of_thursday[index_of_alarm])
-                print(list_of_tuesday[index_of_alarm])
+
                 notifyMe(f"ROZPOCZĘŁA SIĘ {(list_of_thursday[index_of_alarm])} ", "Dołącz na lekcję jak najszybciej!")
             case "FRIDAY":
                 index_of_alarm = lesson_alarm.index(time)
                 print(list_of_friday[index_of_alarm])
-                print(list_of_tuesday[index_of_alarm])
                 notifyMe(f"ROZPOCZĘŁA SIĘ {(list_of_friday[index_of_alarm])} ", "Dołącz na lekcję jak najszybciej!")
 
         sleep(60)
 
 print(list)
 file.close()
-
-
 
 
  
